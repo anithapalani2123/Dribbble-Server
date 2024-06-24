56c80a3a1309a1d473a501241051027119f1a4c8
ANITHA P <94184990+anithapalani2123@users.noreply.github.com>
2024-06-24 22:40:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -454,5 +454,5 @@ app.get('/api/newAndNoteworthy', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(Server running at http://localhost:${port});
+  console.log(`Server running at http://localhost:${port}`);
 });
